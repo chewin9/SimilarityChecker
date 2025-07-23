4736c3662b0e9f5f4b96a75627775f6a217b871f
seungcheol.Lee <cheein9@gmail.com>
2025-07-23 16:38:00 +0900

    modify

--- siron.lee_similarityCheck/similarityChecker_test.cpp
+++ siron.lee_similarityCheck/similarityChecker_test.cpp
@@ -1,7 +1,6 @@
 #include "gmock/gmock.h"
 #include "similarityChecker.cpp"
 using namespace testing;
-<<<<<<< Updated upstream
 
 class LengthFixture : public Test {
 public :
@@ -10,24 +9,6 @@ public :
 	void diagLength(int extpectedScore, string str1, string str2) {
 		int actual = app.getLengthScore(str1, str2);
 		EXPECT_EQ(extpectedScore, actual);
-	}
-};
-
-TEST(TS, EXCEPTION01) {
-	LengthChecker app;
-	try {
-		int ret = app.getLengthScore("", "");
-		FAIL();
-=======
-
-class LengthFixture : public Test {
-public :
-	LengthChecker app;
-
-	void diagLength(int extpectedScore, string str1, string str2) {
-		int actual = app.getLengthScore(str1, str2);
-		EXPECT_EQ(extpectedScore, actual);
->>>>>>> Stashed changes
 	}
 };
 
@@ -53,39 +34,10 @@ TEST_F(ExceptionFixture, EXCEPTIONPARAMETER) {
 	assertIllegalArguemnt("abc", "ab");
 }
 
-<<<<<<< Updated upstream
-TEST(TS, EXCEPTION02) {
-	LengthChecker app;
-	try {
-		int ret = app.getLengthScore("123", "123");
-		FAIL();
-	}
-	catch (exception e) {
-=======
->>>>>>> Stashed changes
-
 TEST_F(LengthFixture, TC01) {
 	diagLength(60, "ABC", "ABC");
 }
 
-<<<<<<< Updated upstream
-TEST(TS, EXCEPTION03) {
-	LengthChecker app;
-	try {
-		int ret = app.getLengthScore("abc", "ab");
-		FAIL();
-	}
-	catch (exception e) {
-
-	}
-}
-
-TEST_F(LengthFixture, TC01) {
-	diagLength(60, "ABC", "ABC");
-}
-
-=======
->>>>>>> Stashed changes
 TEST_F(LengthFixture, TC02) {
 	diagLength(30, "CDE", "AB");
 }
